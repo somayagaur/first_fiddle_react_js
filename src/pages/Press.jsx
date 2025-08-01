359ed64d56771b0bd750b03d8f75621b8544820f
somayagaur <somyagour14@gmail.com>
2025-08-01 23:11:33 +0530

    update

--- src/pages/Press.jsx
+++ src/pages/Press.jsx
@@ -11,7 +11,7 @@ function Press() {
           </div>
         </div>
       </div>
-      <br/><br /><br />
+      <br /><br /><br />
       <div className="container">
         <div className="row">
           <div className="col-md-2"></div>
@@ -23,6 +23,7 @@ function Press() {
           </div>
         </div>
       </div>
+
       <div className="container">
         <div className="row" style={{ margin: '20px 0' }}>
           <div className="col-md-3">
@@ -128,7 +129,7 @@ function Press() {
               {/* 1 */}
               <p className="go ms-2">"Go big, or go home: Lazeez Affaire to Lord of the Drinks, Priyank Sukhija's success recipe for dining</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> JUL 16, 2018</p>
               <h4 className="ms-2">AIN</h4>
@@ -136,23 +137,23 @@ function Press() {
               {/* 2 */}
               <p className="go ms-2">"First Fiddle Restaurants aims top-line growth by 2020; to opt for franchising route to expansion"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> MAY 20, 2019</p>
               <h4 className="ms-2">BUSINESS LINE</h4>
-              
+
               {/* 3 */}
               <p className="go ms-2">"First Fiddle opens outlet in Chennai"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> OCT 20, 2010</p>
               <h4 className="ms-2">BUSINESS STANDARD</h4>
-              
+
               {/* 4 */}
               <p className="go ms-2">"First Fiddle Restaurants aims top-line growth by 2020; to opt for franchising route to expansion"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> NOV 20, 2018</p>
               <h4 className="ms-2">CHENNAI ONLINE</h4>
@@ -160,7 +161,7 @@ function Press() {
               {/* 5 */}
               <p className="go ms-2">"Happy Hour"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> FEB 18, 2020</p>
               <h4 className="ms-2">BUSINESS TODAY</h4>
@@ -168,7 +169,7 @@ function Press() {
               {/* 6 */}
               <p className="go ms-2">"Fly High While Experiencing Newly Launched Dragonfly"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
               <p className="mt-4 ms-2"> AUG 28, 2020</p>
               <h4 className="ms-2">BUSINESS WORLD</h4>
@@ -176,15 +177,18 @@ function Press() {
               {/* 7 */}
               <p className="go">"LORD OF THE DRINKS LAUNCHES IN SOUTH INDIA WITH CHENNAI’S LARGEST RESTO-BAR"</p>
               <p className="read ms-2">READ FULL ARTICLE</p>
-              <hr/>
+              <hr />
 
             </div>
-          </div>  
+          </div>
         </div>
       </div>
 
-            </>
-            )
+
+
+
+    </>
+  )
 }
 
 export default Press;
